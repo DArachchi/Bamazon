aa944502d791371fe0f582db901e3177d1ab87a5
Dino Arachchi <Dino Arachchi>
2017-01-30 19:03:01 -0600

    Added purchase function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,8 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var Table = require('cli-table');
+
+var amountSpent = 0;
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -9,14 +12,49 @@ var connection = mysql.createConnection({
 	database: "Bamazon"
 });
 
-function showInventory() {
-	connection.query("SELECT * FROM products", function(err,res) {
+function purchaseInquiry(){
+	inquirer.prompt([
+		{
+			type: "name",
+			message: "What is the Item ID # of the product you would like to purchase?",
+			name: "itemNumber"
+		},
+		{
+			type: "name",
+			message: "How many would you like to purchase?",
+			name: "quantity"
+		}
+	]).then(function(input) {
+		connection.query("SELECT * FROM products WHERE item_id = ?", [input.itemNumber], function(err, res) {
 			if (err) throw err;
-			console.log("Item ID#  Product Name               Price   Quantity In Stock");
-			for(var i=0; i<res.length; i++) {
-				console.log(res[i].item_id + "         " + res[i].product_name + "       $" + res[i].price + "    " + res[i].stock_quantity);
+			if (res[0].stock_quantity >= input.quantity) {
+				connection.query("UPDATE products SET stock_quantity = ? WHERE item_id =?", [(res[0].stock_quantity)-(input.quantity), input.itemNumber], function(err,response) {
+					showInventory();
+					amountSpent =  input.quantity*res[0].price;
+				});
+			} else {
+				console.log("Insufficient quantity in stock!");
+				purchaseInquiry();
 			}
 		});
+	});
+}
+
+function showInventory() {
+	connection.query("SELECT * FROM products", function(err,res) {
+		if (err) throw err;
+		var table = new Table({
+			head: ["Item ID#", "Product Name", "Price", "Quantity In Stock"],
+			colWidths: [10, 30, 10, 20]
+		});
+		for(var i=0; i<res.length; i++) {
+			table.push(
+				[res[i].item_id, res[i].product_name, "$" + res[i].price, res[i].stock_quantity]
+			);
+		}
+		console.log(table.toString());
+		purchaseInquiry();
+	});
 }
 
 connection.connect(function(err) {
